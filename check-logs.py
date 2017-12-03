d8637ca2538c8bb3988a6efa43a3a99261c1b0cc
Sergey Zelyukin <neo@azdata.net>
2017-12-03 23:32:48 +0400

    cosmetic changes

--- check-logs.py
+++ check-logs.py
@@ -10,7 +10,7 @@ class LogLine(dict):  # line in log file, splitted to columns
     
     if len(LogLine.months_names)==0:    
       for _month_num, _month_name in enumerate(calendar.month_abbr):
-        LogLine.months_names[_month_name] = _month_num 
+        LogLine.months_names[_month_name] = _month_num  # common for all objects
       
     if year:
       self._year = year[0]
@@ -22,10 +22,12 @@ class LogLine(dict):  # line in log file, splitted to columns
     if _m:
       super(LogLine,self).__setitem__("hostname", _m.group(6))
       try:
-        super(LogLine,self).__setitem__("datetime", datetime.datetime.strptime('{0} {1} {2} {3}:{4}:{5}'.format(LogLine.months_names[_m.group(1)],  _m.group(2), str(self._year), _m.group(3), _m.group(4), _m.group(5)), "%m %d %Y %H:%M:%S"))
+        _datetime = datetime.datetime.strptime('{0} {1} {2} {3}:{4}:{5}'.format(LogLine.months_names[_m.group(1)],  _m.group(2), str(self._year), _m.group(3), _m.group(4), _m.group(5)), "%m %d %Y %H:%M:%S")
       except Exception as e:
+        # wrong date
         self._ok = False
         return
+      super(LogLine,self).__setitem__("datetime", _datetime)
     else:
       self._ok = False
 
@@ -38,7 +40,7 @@ class LogLine(dict):  # line in log file, splitted to columns
       self._ok = False
       
   def __setitem__(self):
-    pass
+    pass # do not allow direct changes
       
   def is_reboot(self):
     if not self._ok: return False
